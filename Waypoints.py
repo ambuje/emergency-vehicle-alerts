643374ded1c0e8c1d37f819f6d8503481edc3362
Ambuje Gupta <44320567+ambuje@users.noreply.github.com>
2020-01-31 20:03:53 +0530

    calculate all lat and long

---
+++ Waypoints.py
@@ -0,0 +1,28 @@
+import googlemaps
+from datetime import datetime
+import polyline
+def lat(a,b):
+    gmaps = googlemaps.Client(key='AIzaSyAZQl0TRenJIoCbKNjDKmT2LN9Y94um9qs')
+    now = datetime.now()
+    directions_result = gmaps.directions(a, b,
+                                         mode="driving",
+                                         departure_time=now)
+    final=list()
+    for i in directions_result:
+        #print(i.keys())
+       # print(i['legs'])
+        for j in i['legs']:
+            #print(j.keys())
+            for k in j['steps']:
+                print(k['polyline'])
+                final.append(k['polyline'])
+    
+    finall=[]
+    for p in final:
+        #print(p)
+        #print(p['points'])
+        finall.extend(polyline.decode(p['points']))
+    finall=[(round(item[0],4),round(item[1],4))for item in finall]
+    print((finall))
+    print(len(finall))
+    return finall

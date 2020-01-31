8ac722fa19b24a7eafafabdc516c2f59d19b5199
sonalimeh <36099449+sonalimeh@users.noreply.github.com>
2020-02-01 03:45:01 +0530

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, jsonify,session, redirect, url_for, render_template, abort, request, flash, send_from_directory
+from werkzeug.utils import secure_filename
+from Waypoints import lat
+import googlemaps
+gmaps = googlemaps.Client(key='AIzaSyAZQl0TRenJIoCbKNjDKmT2LN9Y94um9qs') 
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def disp():
+    orig=request.args['origin']
+    dest=request.args['destination']
+    checkpnt=request.args['waypoint']
+    #print(type(orig))
+    orig=orig.split(',')
+    orig=list(map(float,orig))
+    orig=tuple(orig)
+    dest=dest.split(',')
+    dest=list(map(float,dest))
+    dest=tuple(dest)
+
+    checkpnt=checkpnt.split(',')
+    checkpnt=list(map(float,checkpnt))
+    checkpnt=tuple(checkpnt)
+    print(checkpnt)
+    checkpnt=[(item,round(item,4))for item in checkpnt]
+    k=lat(orig,dest)
+    print(k)
+    if checkpnt in k:
+        my_dist = gmaps.distance_matrix(orig,checkpnt)['rows'][0]['elements'][0]
+        destt=my_dist['distance']['text']
+        destt=destt.split(" ")
+        destt=float(destt[0])
+        if destt<=2:
+            return jsonify(True)
+        else:
+            return jsonify(False)
+    else:
+        return jsonify(False)
+if __name__ == '__main__': 
+  
+    app.run(debug = True,port=5000)
